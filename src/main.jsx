484ba829c5558d9716e6481bda4d62dcc3057096
Renas Sahin <rsahin@inf.ethz.ch>
2025-11-21 16:27:03 +0100

    first page implemented, new page named EnergyLevelSelection.jsx created

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import EnergyLevelSelection from './pages/EnergyLevelSelection.jsx'
 import Home from './pages/Home.jsx'
 import Profile from './pages/Profile.jsx'
 import FriendsList from './pages/Friendslist.jsx'
 
-// Bootstrap styles & JS
+// Bootstrap styles
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './app.css'
 
 const router = createBrowserRouter([
-  { path: '/', element: <App />,
+  { path: '/', element: <EnergyLevelSelection /> },
+  { 
+    path: '/home', 
+    element: <App />,
     children: [
       { index: true, element: <Home /> },
       { path: 'profile', element: <Profile /> },
